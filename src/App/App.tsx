6aba663c49a2f7b88cab098927eb8e0eb60f3808
Михаил <111662318+Mikhail2107@users.noreply.github.com>
2025-04-28 10:33:32 +0300

    build

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ApiResponse } from '../interfaces';
 import NearStationList from '../NearStationList/NearStationList';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScheduleNear from '../ScheduleNear/ScheduleNear';
 import MainPage from '../MainPage/MainPage';
 
