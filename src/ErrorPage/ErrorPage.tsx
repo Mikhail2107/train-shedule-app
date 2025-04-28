b79ac6f3c9bc54e6b345580f378c1ae0f71bacd6
Михаил <111662318+Mikhail2107@users.noreply.github.com>
2025-04-28 09:57:44 +0300

    add ErrorPage

---
+++ src/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,15 @@
+
+
+
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+
+    return (
+        <>
+        <h1>PAGE NIT FOUNDED 404</h1>
+        </>
+    );
+};
+
+export default ErrorPage;
--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,13 +1,15 @@
 
 
-import './MainPage';
+import { Link } from 'react-router-dom';
+import './MainPage.css';
 
 
 const MainPage = () => {
 
     return (
         <>
-            
+            <Link to={"/nearest"}>Блиайшие ствнции</Link>
+            <Link to={'/schedule'}>Расписание</Link>
         </>
     );
 };
