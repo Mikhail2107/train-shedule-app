9869ab1278ae43b38b0463dac518c5194b59eac5
Михаил <111662318+Mikhail2107@users.noreply.github.com>
2025-05-07 15:42:50 +0300

    change App - added AppWrapper

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,35 +1,36 @@
+// App.tsx
 import { observer } from 'mobx-react-lite';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NearStationList from '../NearStationList/NearStationList';
 import ScheduleNear from '../ScheduleNear/ScheduleNear';
 import MainPage from '../MainPage/MainPage';
-import { rootStore } from '../stores';
 import ErrorPage from '../ErrorPage/ErrorPage';
-import CheckSation from '../CheckSation/CheckSation';
+import CheckStation from '../CheckSation/CheckSation';
+import { rootStore } from '../stores';
 
-function App() {
+const AppWrapper = observer(() => {
   const { stationStore } = rootStore;
 
   if (stationStore.loading) return <div>Загрузка данных...</div>;
   if (stationStore.geolocationError) return <div>Ошибка геолокации: {stationStore.geolocationError}</div>;
   if (stationStore.error) return <div>Ошибка API: {stationStore.error}</div>;
-  if (!stationStore.data || !stationStore.data.stations) return <div>Нет данных для отображения</div>;
+  if (!stationStore.data?.stations) return <div>Нет данных для отображения</div>;
 
+  return <App />;
+});
+
+function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path={'/'} element={<MainPage />} />
-          <Route path={'/nearest'} element={<NearStationList />}/>
-          <Route path={'/schedule'} element={<ScheduleNear />} />
-          <Route path={'/check-station'} element={<CheckSation />} />
-
-          <Route path={'/*'} element={<ErrorPage />} />
-
-        </Routes>
-      </Router>      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/nearest" element={<NearStationList />} />
+        <Route path="/schedule" element={<ScheduleNear />} />
+        <Route path="/check-station" element={<CheckStation />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default observer(App);
+export default AppWrapper;
