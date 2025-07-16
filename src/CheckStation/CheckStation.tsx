f8a6627c719b4afb5af685dd0858b942d8a8a715
Михаил <111662318+Mikhail2107@users.noreply.github.com>
2025-07-16 10:22:21 +0300

    update css FavoriteStation

--- src/CheckStation/CheckStation.tsx
+++ src/CheckStation/CheckStation.tsx
@@ -1,4 +1,5 @@
 import { observer } from 'mobx-react-lite';
+import {useState} from 'react';
 
 import { rootStore } from '../stores';
 import { Station } from '../interfaces/interfaces';
@@ -9,18 +10,32 @@ import './CheckStation.css';
 const CheckStation = observer(() => {
   const { stationStore } = rootStore;
   const nearestStationList: Station[] | null = stationStore.filteredStations;
+  const [favoriteStations, setFavoriteStations] = useState<Station[]>([])
 
-  console.log(nearestStationList)
+  console.log(favoriteStations)
+
+  const checkedStation = (id:string) => {
+    nearestStationList.filter(station => {
+      if (station.code === id) {
+        setFavoriteStations(prev => [...prev, station])
+      }
+    })
+  }
     return (
       <>
       <h1>Выберите станции</h1>
-      <ul className='checked-station__list'>
+      <div className="checked__box">
+        <ul className='checked-station__list'>
         {nearestStationList.map(station => (
-          <li key={station.code} className='checked-station__item'>
-            <span className='checked-station__title'>{station.title}</span>
+          <li key={station.code} 
+            className='checked-station__item'
+            onClick={() => checkedStation(station.code)}>
+            {station.title}
           </li>
         ))}
       </ul>
+      </div>
+     
       
       </>
     );
